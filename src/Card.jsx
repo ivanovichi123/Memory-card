08e60d8cb62faddbec001f7d1bac1ff027ed7379
ivanovichi <ivan.arthur18@gmail.com>
2026-04-14 08:22:03 -0600

    feat: Added some card dummy content for tests

---
+++ src/Card.jsx
@@ -0,0 +1,65 @@
+import './Card.css'
+
+
+function Card() {
+    const pokemonCards = [
+        {id: 1, Name: "A pokemon", image: "Image of a pokemon"},
+        {id: 2, Name: "Another pokemon", image: "Image of a pokemon"},
+        {id: 3, Name: "A new pokemon", image: "Image of a pokemon"},
+        {id: 4, Name: "Thats a pokemon", image: "Image of a pokemon"},
+    ];
+
+
+
+    return (
+        <main id='theContent'>
+            {pokemonCards.map((individualCard) => {
+                return (
+                    <div key={individualCard.id} className={`Card`}>
+                        <img src="/Dummy.jpg" alt="Pokemon" />
+                        <p className='theName'>{individualCard.Name}</p>
+                    </div>
+                )
+            })}
+        </main>
+    )
+}
+
+export { Card }
+
+//   return (
+//     <div className="theContent" style={{ display: props.theDisplay }}>
+//       {answers.flatMap((answer) => {
+//         if (answer.info === "") {
+//           return [];
+//         } else if (answer.info !== "" && count === 0) {
+//           count += 1;
+//           return (
+//             <div key={answer.id}>
+//               <h2>Practical Experience</h2>
+//               <h3>{answer.title}: </h3>
+//               <p>{answer.info}</p> <br />
+//             </div>
+//           );
+//         } else {
+//           return (
+//             <div key={answer.id}>
+//               <h3>{answer.title}: </h3>
+//               <p>{answer.info}</p> <br />
+//             </div>
+//           );
+//         }
+//       })}
+//     </div>
+//   );
+
+
+
+
+
+
+
+// function Card({state}) {
+//     function dummy() {
+//         state();
+//     }
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import './Content.css'
+import { Card } from './Card.jsx'
 
 function Content() {
+  // const [example, setExample] = useState(0);
+
+  // function theSuperDummy() {
+  //   setExample(example + 1);
+  // }
+
   return (
-    <header> 
-      <div id='theHeader'>
-        <h1 className='theTitle'>Memory card of pokemon</h1>
-        <p className='theSubtitle'>In this game you need to click the cards to earn points without clicking on the same character twice</p>
-        <p id='theScore'>Score: </p>
-        <p id='theBestScore'>Best score: </p>
-      </div>
-    </header>
+    <>
+      <header> 
+        <div id='theHeader'>
+          <h1 className='theTitle'>Memory card of pokemon</h1>
+          <p className='theSubtitle'>In this game you need to click the cards to earn points without clicking on the same character twice</p>
+          <p id='theScore'>Score: </p>
+          <p id='theBestScore'>Best score: </p>
+        </div>
+      </header>
+      <Card />
+    </>
   )
 }
 
