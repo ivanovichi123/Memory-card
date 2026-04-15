e328541e0130be41899d56eba05a720510e4978b
ivanovichi <ivan.arthur18@gmail.com>
2026-04-15 17:52:31 -0600

    feat: All the cards are added

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { useState } from "react";
 import "./Card.css";
 
 function Card(props) { 
@@ -7,47 +8,60 @@ function Card(props) {
     { id: 2, Name: "Another pokemon", image: "Image of a pokemon" },
     { id: 3, Name: "A new pokemon", image: "Image of a pokemon" },
     { id: 4, Name: "Thats a pokemon", image: "Image of a pokemon" },
+    { id: 5, Name: "Yep, a pokemon", image: "Image of a pokemon"},
+    { id: 6, Name: "OMG, a pokemon", image: "Image of a pokemon"},
+    { id: 7, Name: "Pikmin is better", image: "Image of a pokemon"},
+    { id: 8, Name: "Hello i am a pokemon", image: "Image of a pokemon"},
+    { id: 9, Name: "Pokemon", image: "Image of a pokemon"},
+    { id: 10, Name: "A lot of pokemon", image: "Image of a pokemon"},
+    { id: 11, Name: "Many pokemon", image: "Image of a pokemon"},
+    { id: 12, Name: "You can do it", image: "Image of a pokemon"}
   ];
 
-  let newArray = [];
+  let [cards, setCards] = useState(pokemonCards);
 
   function changeOrder() {
-    let copyArray = [...pokemonCards];
+    let copyArray = [...cards];
+    let changeArray = [];
     let maxAmount = copyArray.length;
     let index = 0;
     for(let i = maxAmount; i > 0; i--) {
       index = Math.floor(Math.random() * ((i - 1) - 0 + 1)) + 0;
-      newArray.push(copyArray[index]);
+      changeArray.push(copyArray[index]);
       copyArray.splice(index, 1);
     }
+    setCards(changeArray);
   }
 
   let alreadyUse = useRef([]);
 
-  function changeScore(target) {
-    console.log(alreadyUse.current);
+  function changeScore(id) {
+        const scrollY = window.scrollY; 
     for(let i = 0; i < alreadyUse.current.length; i++) {
-      if(target.id === alreadyUse.current[i]) {
+      if(id === alreadyUse.current[i]) {
         props.theLoseFunction();
         alreadyUse.current = [];
         return;
       }
     }
-    alreadyUse.current.push(target.id);
-    if(alreadyUse.current.length === 4) {
+    alreadyUse.current.push(id);
+    if(alreadyUse.current.length === 12) {
       props.theWinFunction();
       return;
     }
     props.theFunction();
-  }
+    changeOrder();
 
-  changeOrder();
+    requestAnimationFrame(() => {
+      window.scrollTo(0, scrollY);
+    });
+  }
 
   return (
     <main id="theContent">
-      {newArray.map((individualCard) => {
+      {cards.map((individualCard) => {
         return (
-          <div key={individualCard.id} id={individualCard.id} className={`Card`} onClick={(e) => changeScore(e.currentTarget)}>
+          <div key={individualCard.id} className={`Card`} onClick={() => changeScore(individualCard.id)}>
             <img src="/Dummy.jpg" alt="Pokemon" />
             <p className="theName">{individualCard.Name}</p>
           </div>
