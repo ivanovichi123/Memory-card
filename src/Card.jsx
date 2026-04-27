9ef4ac3ff367e32295b99d8884fd23ae36763817
ivanovichi <ivan.arthur18@gmail.com>
2026-04-27 07:37:45 -0600

    feat: Added comments

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,54 +3,69 @@ import { useState, useEffect } from "react";
 import "./Card.css";
 
 function Card(props) {
+
+  //Variable to avoid fetching from the API two times in strict mode
   let fetchDone = useRef(false);
 
+  //Use effect to fetch the images from an API
   useEffect(() => {
+
+    //Declare an async function
     const fetchData = async () => {
+
+      //Check if the fetch has already been done
       if(fetchDone.current === false) {
         fetchDone.current = true;
       } else {
         return;
       }
+
+      //Variable that contain all the pokemon that will be used
       let pokemonInformation = [
         "pikachu", "ditto", "charmander", "sandshrew", "jigglypuff", "paras",
         "psyduck", "slowpoke", "porygon", "natu", "quagsire", "ludicolo"
       ];
+      //Variable that will store the json from the API
       let ultimateInfo = [];
 
       try {
+
+        //Create an array with all the fetches links that will be use
         let information = pokemonInformation.map((pokemonIndividual) => {
           return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIndividual}/`).then(res => res.json());
+        });
 
-      });
-
-        console.log("The info " + information);
-
+        //Promise.all to wait for all the fetches to finish
         let answers = await Promise.all(information);
 
+        //For cycle to get the images from the json and store them in an array
         for (let i = 0; i < answers.length; i++) {
           ultimateInfo.push(answers[i].sprites.other.dream_world.front_default);
         }
 
+        //Change the cards
         setCards(() => {
+          //Copy the default array of pokemon cards
           let update = [...pokemonCardsDefault];
+          //For cycle to change the placeholder text to the actual images
           for (let i = 0; i < ultimateInfo.length; i++) {
             update[i] = {...update[i], image: ultimateInfo[i]};
           }
-          console.log(typeof(update));
+          //Return the value form the function that changes the order of the cards
           return changeOrder(update);
         })
-        console.log(answers);
 
       } catch (error) {
+        //Message if there is an error
         console.error("Error fetching data: ", error);
       }
     };
+
+    //Call the async function
     fetchData();
   }, []);
 
-
-
+  //Default values of the pokemon cards
   const pokemonCardsDefault = [
     { id: 1, Name: "Pikachu", image: "Image of a pokemon" },
     { id: 2, Name: "Ditto", image: "Image of a pokemon" },
@@ -66,48 +81,72 @@ function Card(props) {
     { id: 12, Name: "Ludicolo", image: "Image of a pokemon"}
   ];
 
+  //Variable to store the values of the cards
   let [cards, setCards] = useState(pokemonCardsDefault);
 
+  //Function tha changes the order of the cards
   function changeOrder(array) {
+    //Copy the array
     let copyArray = [...array];
+    //Create new array that stores the new order
     let changeArray = [];
+    //Variables to control the changes in the array
     let maxAmount = copyArray.length;
     let index = 0;
+    //For cycle that changes the order of the array
     for(let i = maxAmount; i > 0; i--) {
+      //Create a random index base on the length of the array
       index = Math.floor(Math.random() * ((i - 1) - 0 + 1)) + 0;
+      //Push the corresponding random index
       changeArray.push(copyArray[index]);
+      //Erase the old index
       copyArray.splice(index, 1);
     }
+    //Return the new array with changes in the order
     return changeArray;
   }
 
+  //Use effect to change the order when first entering the page
   useEffect(() => {
+    //Change the order of the cards based on its current value
     setCards(current => {
       return changeOrder(current);
     });
   }, []);
 
+  //Variable to check which cards have already been click
   let alreadyUse = useRef([]);
 
+  //Function that changes the score
   function changeScore(id) {
+    //Store the value of the scroll to to prevent it from going up after each click 
     const scrollY = window.scrollY; 
+    //For cycle to check if the clicked card has not already been clicked
     for(let i = 0; i < alreadyUse.current.length; i++) {
       if(id === alreadyUse.current[i]) {
+        //If the card was already clicked lose the points
         props.theLoseFunction();
+        //Reset the array of already clicked cards
         alreadyUse.current = [];
         return;
       }
     }
+    //Push the id as an already use card
     alreadyUse.current.push(id);
+    //Check if the already use array is the total length of the total cards
     if(alreadyUse.current.length === 12) {
+      //Win the game
       props.theWinFunction();
       return;
     }
+    //Increment one score
     props.theFunction();
+    //Change the order of the cards
     setCards(current => {
       return changeOrder(current);
     });
 
+    //Make the scroll stay in its place without going up
     requestAnimationFrame(() => {
       window.scrollTo(0, scrollY);
     });
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,29 +1,38 @@
-import { useRef } from "react";
 import { useState } from "react";
 import "./Content.css";
 import { Card } from "./Card.jsx";
 
 function Content() {
+
+  //Variables for the score
   const [score, setScore] = useState(0);
   const [gameText, setGameText] = useState("You can do it..");
   const [bestScore, setBestScore] = useState(0);
 
+  //Function that changes the score
   function scoreChanges() {
     setScore(score + 1);
+    //If score is bigger than the best score
     if(score >= bestScore) {
+      //Increment best score
       setBestScore(bestScore + 1);
     }
     setGameText("You can do it..");
   }
 
+  //Function that changes score when you lose
   function scoreLose() {
+    //Set score to zero
     setScore(0);
     setGameText("You lose..");
   }
 
+  //Function that handles winning the game
   function wonGame() {
     setScore(score + 1);
+    //If score is bigger than the best score
     if(score >= bestScore) {
+      //Increment best score
       setBestScore(bestScore + 1);
     }
     setGameText("You won!!");
